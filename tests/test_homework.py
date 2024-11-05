f73e58d002f73fa1027a828bb6f0c72a512935af
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2024-11-05 10:55:35 -0500

    Initial commit

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -3,29 +3,33 @@
 import os
 
 import pandas as pd  # type: ignore
+
 from homework.word_count import run_job
 
 
 def test_01():
     """Test word count job."""
 
-    run_job("input/", "output")
+    run_job(
+        "files/input/",
+        "files/output/",
+    )
 
     #
     # Retorna error si el archivo "_SUCCESS" no existe en la
     # carpeta output/
-    if not os.path.exists("output/_SUCCESS"):
-        raise FileNotFoundError("File 'output/_SUCCESS' not found")
+    if not os.path.exists("files/output/_SUCCESS"):
+        raise FileNotFoundError("File 'files/output/_SUCCESS' not found")
 
     #
     # Retorna error si la carpeta output/ no existe
-    if not os.path.exists("output/part-00000"):
-        raise FileNotFoundError("File 'output/part-00000' not found")
+    if not os.path.exists("files/output/part-00000"):
+        raise FileNotFoundError("File 'files/output/part-00000' not found")
 
     #
     # Lee el contenido del archivo output.txt
     dataframe = pd.read_csv(
-        "output/part-00000",
+        "files/output/part-00000",
         delimiter="\t",
         header=None,
         names=["word", "count"],
